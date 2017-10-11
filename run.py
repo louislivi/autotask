c9b72ad1cc6b68796a9230bd1e3a45b88bce85ae
louislivi <574747417@qq.com>
2017-10-11 23:55:30 +0800

    1.2
    1.2

--- run.py
+++ run.py
@@ -3,22 +3,22 @@
 
 import urllib2
 import urllib
-import zipfile
 import re
 import time
 import os
 from unrar import rarfile
-import inspect, os
-from docx import Document
-
+import inspect
+from wordChangeTxt import Translate
+import codecs
 #登录的用户名和密码
 username = "cqliwei321"
 password = "liwei123"
 url="http://learning.cmr.com.cn/student/acourse/HomeworkCenter/index.asp?courseid=zk133a"
 
-def getHtmlSource(url, username, password):
+def getHtmlSource(url, username, password, data = {}):
     try:
-        # 创建一个密码管理者  
+	
+		# 创建一个密码管理者  
         password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()  
         # 添加用户名和密码  
         password_mgr.add_password(None, url, username, password)  
@@ -30,9 +30,15 @@ def getHtmlSource(url, username, password):
         opener.open(url)  
         # 安装 opener.  
         urllib2.install_opener(opener)  
-        #urllib2.urlopen 使用上面的opener.  
-        ret = urllib2.urlopen(url)
-
+		# post数据
+	if data:
+		user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'# 将user_agent写入头信息
+		headers = { 'User-Agent' : user_agent }
+		data = urllib.urlencode(data)
+		url = urllib2.Request(url, data, headers)
+	#urllib2.urlopen 使用上面的opener.  
+	ret = urllib2.urlopen(url)
+	#print ret.headers
         return ret.read()
     except urllib2.HTTPError, e:
         if e.code == 401:
@@ -43,36 +49,75 @@ def getHtmlSource(url, username, password):
         return None
 		
 def downloadTask(html):
-	regex_content = re.compile(
-            '<div.*?class="button_blue2".*?href=\"(.+?)\"',
-            re.S)
-	items = re.findall(regex_content, html)
-	print "downloading with urllib"
-	filedir  = os.path.join(script_path(),"task_"+time.strftime('%Y%m%d%H%I%S'))  #解压后放入的目录
-	if os.path.isdir(filedir):
-		pass
-	else:
-		os.mkdir(filedir)  
-	rar_path = os.path.join(filedir,"task.rar")
-	urllib.urlretrieve(items[0], rar_path)
-	print "download finish!"
-	print "unrar......!"
-	file = rarfile.RarFile(rar_path)  #这里写入的是需要解压的文件，别忘了加路径
-	file.extractall(filedir)  #这里写入的是你想要解压到的文件夹
-	print "unrar finish!"
-	filelist=os.listdir(filedir)
-	for i in filelist:
-		fullfile = os.path.join(filedir,i)
-		if not os.path.isdir(fullfile):
-			if ".doc" in i:    #1.txt为你所查找的文件名
-				document = Document(os.path.join(script_path(),"task_20171011010142\ZK133A.docx"))
-				for p in document.paragraphs:
-					print p.text
-				print docText
-				print fullfile
-			else:
-				pass
+	# regex_content = re.compile(
+            # '<div.*?class="button_blue2".*?href=\"(.+?)\"',
+            # re.S)
+	# items = re.findall(regex_content, html)
+	# print "downloading with urllib"
+	# filedir_child  = os.path.join(script_path(),"task")  #解压后放入的目录
+	# filedir  = os.path.join(filedir_child,"task_"+time.strftime('%Y%m%d%H%I%S'))  #解压后放入的目录
+	# if not os.path.isdir(filedir_child): os.mkdir(filedir_child)  
+	# if not os.path.isdir(filedir): os.mkdir(filedir)
+	# rar_path = os.path.join(filedir,"task.rar")
+	# urllib.urlretrieve(items[0], rar_path)
+	# print "download finish!"
+	# print "unrar......!"
+	# file = rarfile.RarFile(rar_path)  #这里写入的是需要解压的文件，别忘了加路径
+	# file.extractall(filedir)  #这里写入的是你想要解压到的文件夹
+	# print "unrar finish!"
+	# answer_path = Translate(filedir)
+	#返回答案结果
 	
+	#answer = open(answer_path,'r')
+	answer = open(os.path.join(script_path(),"task\\task_20171011200841\\data\\ZK133A.txt"),'r')
+	#print(answer.readlines().encode('utf8'))
+	reader = codecs.getreader('gbk')(answer)
+	return reader.read()
+	
+def get_question(answer_data, task_html, task_url):
+	#匹配问题列表
+	task_key_word = u'做作业'
+	regex_content = re.compile(
+            '<div.*?class=\"button_red\".*?<a.*?href=\"(.+?)\".*?class=\"a\_white\".*?>'+task_key_word.encode('gbk')+'</a>',
+            re.S)
+	items = re.findall(regex_content, task_html.encode('gbk'))
+	for item in items:
+		#匹配单个问题url
+		question_html = getHtmlSource(task_url+item, username, password)
+		question_regex = u'【(.*?)】'
+		regex_content = re.compile(
+            question_regex.encode('gbk'),
+            re.S)
+		question_num_items = re.findall(regex_content, question_html.encode('gbk'))
+		answer = {}
+		for question_num_item in question_num_items:
+			#匹配问题答案
+			answer_regex = u'案】'
+			regex_content = re.compile(
+				question_num_item+'.*?'+answer_regex.encode('gbk')+'([A-Z])',
+				re.S)
+			answer_items = re.findall(regex_content, answer_data.encode('gbk'))
+			answer[question_num_item] = answer_items
+			
+		#获取提交答案路径
+		regex_content = re.compile(
+            '<form.*?id="form1".*?name="form1".*?action="(.*?)"',
+            re.S)
+		post_question_url_items = re.findall(regex_content, question_html.encode('gbk'))
+		post_question_url = task_url+post_question_url_items[0]
+		#创建post体
+		data = answer
+		data['CourseID'] = re.findall(re.compile('<input.*?name=\"CourseID\".*?value=\"(.*?)\"',re.S),question_html)[0]
+		data['PMID']     = re.findall(re.compile('<input.*?name=\"PMID\".*?value=\"(.*?)\"',re.S),question_html)[0]
+		data['tmpSID']   = re.findall(re.compile('<input.*?name=\'tmpSID\'.*?value=\'(.*?)\'',re.S),question_html.encode('gbk'))[0]
+		data['strStandardScore'] = re.findall(re.compile('<input.*?name=\'strStandardScore\'.*?value=\'(.*?)\'',re.S),question_html)[0]
+		#post提交答案
+		result = getHtmlSource(post_question_url, username, password, data)
+		print data
+		print result
+		return 
+
+
 def script_path():
 	caller_file = inspect.stack()[1][1]         # caller's filename
 	return os.path.abspath(os.path.dirname(caller_file))# path
@@ -89,6 +134,8 @@ def run():
             re.S)
 	task_url_items = re.findall(regex_content, url)
 	task_html = getHtmlSource(task_url_items[0]+task_url, username, password)
-	return downloadTask(task_html)
-
-print run()
+	answer_data = downloadTask(task_html)
+	get_question(answer_data,task_html,task_url_items[0])
+	
+if __name__ == '__main__': 
+	print run()
---
+++ wordChangeTxt.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+''''' 
+Created on 2015年11月21日 
+ 
+@author: lushangqi 
+'''  
+#!D:\\My Python\\Trans_to_txt.py  
+#注意Windows下路径表示   
+from win32com import client as wc  
+import os  
+import fnmatch  
+  
+  
+all_FileNum = 0  
+debug = 0  
+  
+  
+def Translate(path):  
+    ''''' 
+    将一个目录下所有doc和docx文件转成txt 
+    该目录下创建一个新目录data 
+    新目录下fileNames.txt创建一个文本存入所有的word文件名 
+    本版本具有一定的容错性，即允许对同一文件夹多次操作而不发生冲突 
+    '''  
+    global debug, all_FileNum  
+    if debug:  
+        print path  
+    #该目录下所有文件的名字 
+    files = os.listdir(path)   
+    #该目下创建一个新目录data，用来放转化后的txt文本  
+    New_dir = os.path.abspath(os.path.join(path, 'data'))  
+    if not os.path.exists(New_dir):  
+        os.mkdir(New_dir)  
+    if debug:  
+        print New_dir  
+    #创建一个文本存入所有的word文件名  
+    fileNameSet= os.path.abspath(os.path.join(New_dir, 'fileNames.txt'))  
+    o=open(fileNameSet,"w")   
+    try:  
+        for filename in files:  
+            if debug:  
+                print filename  
+            #如果不是word文件：继续  
+            if not fnmatch.fnmatch(filename, '*.doc') and not fnmatch.fnmatch(filename, '*.docx'):  
+                continue;  
+            #如果是word临时文件：继续  
+            if fnmatch.fnmatch(filename, '~$*'):  
+                continue;  
+            if debug:  
+                print filename  
+            docpath = os.path.abspath(os.path.join(path,filename))  
+              
+            #得到一个新的文件名,把原文件名的后缀改成txt  
+            new_txt_name = ''  
+            if fnmatch.fnmatch(filename, '*.doc'):      
+                new_txt_name = filename[:-4]+'.txt'  
+            else:  
+                new_txt_name = filename[:-5]+'.txt'  
+            if debug:  
+                print new_txt_name  
+            word_to_txt = os.path.join(os.path.join(path, 'data'),new_txt_name)    
+            wordapp = wc.Dispatch('Word.Application')  
+            doc = wordapp.Documents.Open(docpath)  
+            #为了让python可以在后续操作中r方式读取txt和不产生乱码，参数为4  
+            doc.SaveAs(word_to_txt,4)  
+            doc.Close()  
+            o.write(word_to_txt+'\n')  
+            all_FileNum += 1  
+    finally:  
+        wordapp.Quit()  
+	return word_to_txt
+  
+if __name__ == '__main__':  
+    print('Enter your Director\'s path:')  
+    print("path use\ or \\ all ok!")  
+    mypath = raw_input()  
+    print ('touch file have:')  
+    Translate(mypath)  
+    print 'The Total Files Numbers = ', all_FileNum  
